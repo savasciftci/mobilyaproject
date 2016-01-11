f3f8c9c2b0b510f19627888bd163716396d75faa
savashan0019 <savasciftci1905@gmail.com>
2016-01-11 15:35:26 +0200

    son hal

--- app/views/Template_BackEnd/left_view.php
+++ app/views/Template_BackEnd/left_view.php
@@ -2,13 +2,17 @@
 
 <aside class="main-sidebar">
 
-    
+
     <section class="sidebar">
 
         <!-- Sidebar user panel -->
         <div class="user-panel">
             <div class="pull-left image">
-                <img src="<?php echo SITE_PROFILRESIM; ?>/<?php echo Session::get("presim"); ?>" class="img-circle" alt="User Image">
+                <?php if (Session::get("presim")) { ?>
+                    <img src="<?php echo SITE_PROFILRESIM; ?>/<?php echo Session::get("presim"); ?>" class="img-circle" alt="User Image">
+                <?php } else { ?>
+                    <img src="<?php echo SITE_BACK_ASSETS_DISTIMG; ?>/user2-160x160.jpg" class="img-circle" alt="User Image">
+                <?php } ?> 
             </div>
             <div class="pull-left info">
                 <p><?php echo Session::get("kadi"); ?></p>
@@ -19,28 +23,19 @@
         <ul class="sidebar-menu">
 
             <li>
-                <a href="#">
-                    <i class="fa fa-dashboard"></i> <span>ÜRÜNLER</span> <i class="fa fa-angle-left pull-right"></i>
+                <a href="<?php echo SITE_URLA; ?>/Urunler">
+                    <i class="fa fa-dashboard"></i> <span>ÜRÜNLER</span> <i class=" pull-right"></i>
                 </a>
-                <ul class="treeview-menu">
-                    <li class="active"><a href="<?php echo SITE_URLA; ?>/Urunler"><i class="fa fa-circle-o"></i> Yeni Ürün Ekle</a></li>
-                    <li class="active"><a href="index2.html"><i class="fa fa-circle-o"></i> Ürün Düzenle/Sil</a></li>
-                </ul>
+               
             </li>
             <li>
-                <a href="#">
-                    <i class="fa fa-dashboard"></i> <span>KATEGORİLER</span> <i class="fa fa-angle-left pull-right"></i>
+                <a href="<?php echo SITE_URLA; ?>/Kategoriler">
+                    <i class="fa fa-dashboard"></i> <span>KATEGORİLER</span> <i class=" pull-right"></i>
                 </a>
-                <ul class="treeview-menu">
-                    <li class="active"><a href="<?php echo SITE_URLA; ?>/Kategoriler"><i class="fa fa-circle-o"></i> Yeni Kategori Ekle</a></li>
-                    <li><a href="index2.html"><i class="fa fa-circle-o"></i> Kategori Düzenle/Sil</a></li>
-                </ul>
+               
             </li>
             <li><a href="<?php echo SITE_URLA; ?>/GenelAyarlar"><i class="fa fa-cog"></i> <span>GENEL AYARLAR</span></a></li>
-            <div class="modal-footer">
-                <a href="<?php echo SITE_URL;?>/login/logout"><button type="button"  class="btn btn-warning" data-dismiss="modal">Çıkış Yap</button></a>>
-            </div>
-         
+
         </ul>
 
     </section>
